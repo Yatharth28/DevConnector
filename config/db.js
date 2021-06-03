491e5561b15863baf028b67b23e9c73affe54c47
Yatharth28 <55477047+Yatharth28@users.noreply.github.com>
2021-06-03 12:04:10 +0530

    user api and jwt

--- config/db.js
+++ config/db.js
@@ -1,14 +1,15 @@
-const mongoose = require("mongoose");
-const config = require("config");
-const db = config.get("mongoURI"); //this gets the address from default.json
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI'); //this gets the address from default.json
 
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      useCreateIndex: true,
     });
-    console.log("MongoDB connected...");
+    console.log('MongoDB connected...');
   } catch (err) {
     console.error(err.message);
     //exit process with failure

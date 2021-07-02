9e8aa8072a621a9eb42330b363e33155bae86c29
Yatharth28 <55477047+Yatharth28@users.noreply.github.com>
2021-07-02 17:45:57 +0530

    experience & education added

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,8 +8,14 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
+        <Link to='/dashboard'>
+          <i className='fas fa-user' />{' '}
+          <span className='hide-sm'> Dashboard</span>
+        </Link>
+      </li>
+      <li>
         <a onClick={logout} href='#!'>
-          <i className='fas fa-sign-out-alt'></i>{' '}
+          <i className='fas fa-sign-out-alt' />{' '}
           <span className='hide-sm'> Logout</span>
         </a>
       </li>
@@ -19,7 +25,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const guestLinks = (
     <ul>
       <li>
-        <a href='#!'>Developers</a>
+        <a href='#!\'>Developers</a>
       </li>
       <li>
         <Link to='/register'>Register</Link>
